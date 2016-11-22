29ecdc39f7dbe8a293015806387acf0b65291789
nattapon <nattapon@nattapons-MacBook-Pro.local>
2016-11-22 15:07:30 +0700

    add nginx self-sign certificate

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -12,3 +12,35 @@ yum_package 'nginx' do
    action :install
 end
 
+service "nginx" do
+  provider Chef::Provider::Service::Systemd
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable ]
+end
+
+
+directory "/etc/nginx/ssl" do
+  recursive true
+end
+%w[ private_key.pem self_cert.pem ].each do |keyfile|
+  cookbook_file "/etc/nginx/ssl/#{keyfile}" do
+    source "#{keyfile}"
+    action :create
+  end
+end
+
+
+Chef::Log.info("********** The instance private_ip is '#{elastic['private_ip']}' **********")
+template "/etc/kibana/kibana.yml" do
+  source "kibana.yml.erb"
+  variables(
+    :host => "#{elastic[:private_ip]}"
+  )
+  notifies :restart, "service[kibana]", :immediately
+end
+
+
+
+
+
+
